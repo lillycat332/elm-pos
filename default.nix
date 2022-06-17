ed4707421b9051b85e996b3f9ae8c178cef9dd20
Lilly <54189319+lillycat332@users.noreply.github.com>
2022-06-17 12:52:34 +0100

    elm-pos: add nix build file

---
+++ default.nix
@@ -0,0 +1,27 @@
+let
+  pkgs = import <nixpkgs> { };
+  stdenv = pkgs.llvmPackages_13.stdenv;
+in
+
+pkgs.stdenv.mkDerivation
+{
+  name = "elm-pos";
+  version = "0.1.0";
+
+  buildInputs = with pkgs; [
+    clang
+    zlib
+    elmPackages.elm
+    sqlite
+  ];
+
+  buildPhase = ''
+    elm make --optimized
+  '';
+
+  meta = {
+    description = "PoS (point of sale) frontend in elm";
+    license = "BSD";
+  };
+}
+
